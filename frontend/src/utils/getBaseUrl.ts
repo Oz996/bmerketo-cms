c616e7f500be1fff0e5a3271328240c31d3e2d01
Oz996 <ozgun996@hotmail.com>
2024-03-17 13:24:20 +0100

    Update getBaseUrl.ts

--- frontend/src/utils/getBaseUrl.ts
+++ frontend/src/utils/getBaseUrl.ts
@@ -1,15 +1,10 @@
 export const getBaseUrl = () => {
   if (typeof window !== "undefined") {
-    // Client-side code
+    // fetch from our local backend if browsing locally, or fetch from hosted service if not
     console.log("window", window);
-    if (window.location.host === "http://localhost:5173")
+    if (window.location.host === "localhost:5173") {
       return "http://localhost:7000";
-    return "https://cms-api-ty0d.onrender.com"; // or return any default client URL
-  } else if (process.env.NODE_ENV === "production") {
-    // Running in production
-    return "https://cms-api-ty0d.onrender.com"; // Update with your production API URL
-  } else {
-    // Running locally or in a development environment
-    return "http://localhost:7000"; // Update with your local development API URL
+    }
+    return "https://cms-api-ty0d.onrender.com";
   }
 };
