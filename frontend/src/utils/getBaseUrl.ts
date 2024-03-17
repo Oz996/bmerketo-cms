f52ed2c9a688aa5b8b62353c36673d8c8541537c
Oz996 <ozgun996@hotmail.com>
2024-03-17 13:20:08 +0100

    Update getBaseUrl.ts

--- frontend/src/utils/getBaseUrl.ts
+++ frontend/src/utils/getBaseUrl.ts
@@ -4,7 +4,7 @@ export const getBaseUrl = () => {
     console.log("window", window);
     if (window.location.host === "http://localhost:5173")
       return "http://localhost:7000";
-    return ""; // or return any default client URL
+    return "https://cms-api-ty0d.onrender.com"; // or return any default client URL
   } else if (process.env.NODE_ENV === "production") {
     // Running in production
     return "https://cms-api-ty0d.onrender.com"; // Update with your production API URL
