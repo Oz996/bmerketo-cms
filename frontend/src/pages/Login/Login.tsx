a977a8beb82d209a81670d47bc060837e5bfb396
Oz996 <ozgun996@hotmail.com>
2024-03-19 21:51:54 +0100

    Proper form validation on login instead of toast

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -6,37 +6,58 @@ import { useAuth } from "../../hooks/useAuth";
 import { toast } from "react-toastify";
 import { AiOutlineArrowRight } from "react-icons/ai";
 import { getBaseUrl } from "../../utils/getBaseUrl";
+import { BiError } from "react-icons/bi";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState({ value: "", error: "" });
+  const [password, setPassword] = useState({ value: "", error: "" });
   const [isLoading, setIsLoading] = useState(false);
 
   const { handleLogin } = useAuth();
 
   const navigate = useNavigate();
 
+  console.log(email.value);
+
   const loginAdmin = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    if (!email && !password) return toast.error("Fill out all the fields");
+    // form error handling
+    if (!email.value && !password.value) {
+      setEmail({ value: "", error: "Please enter a email address" });
+      setPassword({ value: "", error: "Please enter your password" });
+      return;
+    }
+    if (!email.value && password.value)
+      return setEmail({ ...email, error: "Please enter a email address" });
+    if (!password.value && email.value)
+      return setPassword({ ...password, error: "Please enter your password" });
+
+    if (!/^\S+@\S+$/.test(email.value)) {
+      setEmail({ ...email, error: "Invalid email" });
+      return;
+    }
 
     try {
       setIsLoading(true);
+      const dataObject = {
+        email: email.value,
+        password: password.value,
+      };
       const res = await fetch(getBaseUrl() + "/api/login/admin", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ email, password }),
+        body: JSON.stringify(dataObject),
       });
       if (res.ok) {
         const data = await res.json();
         const token = data.token;
-        handleLogin(token, email);
+        handleLogin(token, email.value);
         navigate("/overview");
       } else {
-        toast.error("Unable to login");
+        setEmail({ ...email, error: "Incorrect credentials" });
       }
     } catch (error) {
       console.error(error);
@@ -54,19 +75,33 @@ const Login = () => {
             Add, delete or edit products and orders as an admin
           </p>
           <input
-            type="email"
+            className={email.error && "login-input-error"}
+            type="text"
             placeholder="Email"
             name="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            value={email.value}
+            onChange={(e) => setEmail({ value: e.target.value, error: "" })}
           />
+          {email.error && (
+            <div className="login-error-div">
+              <BiError size={20} style={{ color: "red" }} />
+              <p>{email.error}</p>
+            </div>
+          )}
           <input
+            className={password.error && "login-input-error"}
             type="password"
             placeholder="Password"
             name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            value={password.value}
+            onChange={(e) => setPassword({ value: e.target.value, error: "" })}
           />
+          {password.error && (
+            <div className="login-error-div">
+              <BiError size={20} style={{ color: "red" }} />
+              <p>{password.error}</p>
+            </div>
+          )}
           {isLoading ? (
             <button
               disabled
